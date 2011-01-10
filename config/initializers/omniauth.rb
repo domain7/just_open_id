e67866b3e3eaef72630f560bd650f9663a2195cb
Jody Alkema <jody@alkema.ca>
2011-01-09 20:46:01 -0800

    0.0.3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,9 @@
 require 'openid/store/filesystem'
-# require 'openid/store/memcache'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # options = {}
+  # options[:identifier] = JustOpenId::Engine.config.openid_url if JustOpenId::Engine.config.openid_url
   provider :open_id, OpenID::Store::Filesystem.new('./tmp')
-  # provider :open_id, OpenID::Store::Memcache.new(Dalli::Client.new)
-  # provider :open_id, ActiveRecordStore.new, :identifier => JustOpenId::Engine.config.openid_url
 end
 
 
